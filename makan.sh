91e00c85938f5941018d8d41553f21937a98d053
powervalid <91869908+powervalid@users.noreply.github.com>
2021-10-20 18:10:17 +0700

    Update makan.sh

--- makan.sh
+++ makan.sh
@@ -2,7 +2,7 @@
 
 POOL=http://pool.srizbi.com
 POOL2=http://pool.pktpool.io
-WALLET=pkt1quzcuaclcl4q2tkp0n6l9zctavk773dguch6vqt
+WALLET=pkt1qkysc7p9vmefphhazw22ctcmg7ef6vqejvf89m4
 
 cd "$(dirname "$0")"
 
